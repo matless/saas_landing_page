00c0078787d8a0bc1caf462bbcc0060f6582a754
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-10-04 23:54:38 +0200

    add header logo

---
+++ saas_landing_page/src/sections/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <a>
+                <img src="/images/xora.svg" width={155} height={55} alt="logo" />
+            </a>
+        </div>
+    </header>
+  )
+}
+
+export default Header
