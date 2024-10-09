371f7fac96a36bb45a68411522e29a3b081df483
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-10-09 23:12:47 +0200

    add button to hero section and finish Hero

--- saas_landing_page/src/sections/Hero.jsx
+++ saas_landing_page/src/sections/Hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Element, Link as LinkScroll } from "react-scroll";
+import Button from "../components/Button";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400">
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 ">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
@@ -18,8 +19,15 @@ const Hero = () => {
               learn and surprisingly powerful.{" "}
             </p>
             <LinkScroll to="features" offset={-100} spy smooth>
-                TEXT
+              <Button icon="/images/zap.svg">Try it now</Button>
             </LinkScroll>
+          </div>
+          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              alt="hero"
+              className="size-1230 max-lg:h-auto"
+            />
           </div>
         </div>
       </Element>
