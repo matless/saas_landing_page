b74b08693a572c5d1b46d86924b266964bc6ee40
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-10-26 19:34:35 +0200

    add faq section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import Header from './sections/Header'
-import Hero from './sections/Hero'
-import Features from './sections/Features'
-import Pricing from './sections/Pricing'
+import React from "react";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
 
 const App = () => {
   return (
@@ -11,8 +12,9 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <Faq />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/sections/Faq.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Element } from "react-scroll";
+
+const Faq = () => {
+  return (
+    <section>
+      <Element name="faq" className="relative">
+        <div>
+          <h3>Curiosity didn't kill the cat, it gave it answers.</h3>
+          <p>You've got questions, we've got answers.</p>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Faq;
