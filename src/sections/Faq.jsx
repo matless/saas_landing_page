66565c055c91a4c7c51177f847e0f208ecfa8755
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-10-29 19:51:19 +0100

    add logo to faq section

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Element } from "react-scroll";
 
 const Faq = () => {
@@ -13,7 +12,14 @@ const Faq = () => {
             <p className="body-1 max-lg:max-w-sm">
               You've got questions, we've got answers.
             </p>
-          <div className="faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2 "/>
+          </div>
+          <div className="faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2 " />
+        </div>
+        <div className="faq-glow_before relative z-2 border-2 border-s2 bg-s1 ">
+          <div className="container flex gap-10 max-lg:block">
+            <div className="rounded-half absolute -top-10  left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1">
+              <img src="/images/faq-logo.svg" alt="logo" className="size-1/2" />
+            </div>
           </div>
         </div>
       </Element>
