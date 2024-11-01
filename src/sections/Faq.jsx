a5e864af72a586ca940ccb871539806e1c1f69c5
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-11-01 22:56:55 +0100

    add FaqItem

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,5 +1,6 @@
 import { Element } from "react-scroll";
 import {faq} from '../constants/index'
+import FaqItem from "../components/FaqItem";
 
 const Faq = () => {
 
@@ -25,8 +26,8 @@ const Faq = () => {
             </div>
 
             <div className="relative flex-1 pt-24">
-              {faq.slice(0, halfLength).map((faq, index) => (
-                <div>{faq.question}</div>
+              {faq.slice(0, halfLength).map((item, index) => (
+                <FaqItem key={item.id} item={item} index={index} />
               ))}
             </div>
           </div>
